a0a1aa00e2c5a97e8becf0537bcb0b3f3f9eb427
Wouter <wouter.kerkhofs@telenet.be>
2018-04-12 21:57:04 +0200

    parity-kata

---
+++ C-sharp/ParityOutlier/TheCode/Kata.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TheCode
+{
+    public class Kata
+    {
+        /*
+            You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+            The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
+            Write a method that takes the array as an argument and returns this "outlier" N.
+         */
+
+        public static int Find(int[] integers)
+        {
+            int? result = null;
+            var odds = new List<int>();
+            var evens = new List<int>();
+            for (int i = 0; i < integers.Length && !result.HasValue; i++)
+            {
+                int currInteger = integers[i];
+                if(currInteger % 2 ==0)
+                    evens.Add(currInteger);
+                else 
+                    odds.Add(currInteger);
+
+                if (evens.Count > 1)
+                {
+                    //N is odd
+                    result = odds.Any() ? odds[0] : FindParity(integers, false);
+                } else if (odds.Count > 1)
+                {
+                    //N is even
+                    result = evens.Any() ? evens[0] : FindParity(integers, true);
+                }
+                //else: we can' determine N yet
+            }
+
+            return result ?? -1;
+        }
+
+        private static int? FindParity(int[] integers, bool findEven)
+        {
+            return integers.Single(i => (i % 2 == 0) == findEven);
+        }
+    }
+}
---
+++ C-sharp/ParityOutlier/UnitTestProject/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using TheCode;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public static void Test1()
+        {
+            int[] exampleTest1 = { 2, 6, 8, -10, 3 };
+            Assert.IsTrue(3 == Kata.Find(exampleTest1));
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            int[] exampleTest2 = { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 };
+            Assert.IsTrue(206847684 == Kata.Find(exampleTest2));
+        }
+
+        [Test]
+        public static void Test3()
+        {
+            int[] exampleTest3 = { int.MaxValue, 0, 1 };
+            Assert.IsTrue(0 == Kata.Find(exampleTest3));
+        }
+    }
+}
