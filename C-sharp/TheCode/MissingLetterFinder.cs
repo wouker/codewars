eed3276a8320abdb10e0e20808db0228c820ac18
Wouter <wouter.kerkhofs@telenet.be>
2018-04-14 12:52:38 +0200

    missingletter-kata

---
+++ C-sharp/TheCode/MissingLetterFinder.cs
@@ -0,0 +1,13 @@
+﻿namespace TheCode
+{
+    public class MissingLetterFinder
+    {
+        public static char Find(char[] array)
+        {
+            var expectedCounter = (int)array[0];
+            var realityCounter = 1;
+            while (++expectedCounter == array[realityCounter++]) { }
+            return (char)expectedCounter;
+        }
+    }
+}
---
+++ C-sharp/UnitTestProject/MissingLetterTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using TheCode;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class MissingLetterTests
+    {
+        [Test]
+        public void MissingLetterTests_TestLower()
+        {
+            Assert.AreEqual('e', MissingLetterFinder.Find(new[] { 'a', 'b', 'c', 'd', 'f' }));
+        }
+
+        [Test]
+        public void MissingLetterTests_TestUpper()
+        {
+            Assert.AreEqual('P', MissingLetterFinder.Find(new[] { 'O', 'Q', 'R', 'S' }));
+        }
+    }
+}
