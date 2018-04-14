223c52fbe28d5c7dbfae3f0cbcc189cd06fc32e3
Wouter <wouter.kerkhofs@telenet.be>
2018-04-14 12:22:43 +0200

    rename tests

--- C-sharp/UnitTestProject/ParityOutlierTests.cs
+++ C-sharp/UnitTestProject/ParityOutlierTests.cs
@@ -7,21 +7,21 @@ namespace UnitTestProject
     public class ParityOutlierTests
     {
         [Test]
-        public static void Test1()
+        public static void ParityOutlierTests_Test1()
         {
             int[] exampleTest1 = { 2, 6, 8, -10, 3 };
             Assert.IsTrue(3 == ParityOutlier.Find(exampleTest1));
         }
 
         [Test]
-        public static void Test2()
+        public static void ParityOutlierTests_Test2()
         {
             int[] exampleTest2 = { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 };
             Assert.IsTrue(206847684 == ParityOutlier.Find(exampleTest2));
         }
 
         [Test]
-        public static void Test3()
+        public static void ParityOutlierTests_Test3()
         {
             int[] exampleTest3 = { int.MaxValue, 0, 1 };
             Assert.IsTrue(0 == ParityOutlier.Find(exampleTest3));
