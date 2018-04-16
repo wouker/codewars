955f0fa8bc04bce0b2ec416df39332b32321a5d6
Wouter <wouter.kerkhofs@telenet.be>
2018-04-16 18:52:54 +0200

    oddsort

---
+++ C-sharp/TheCode/OddSort.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace TheCode
+{
+    public class OddSort
+    {
+        /*
+         You have an array of numbers.
+         Your task is to sort ascending odd numbers but even numbers must be on their places.
+         Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+         */
+
+        public static int[] SortArray(int[] array)
+        {
+            var list = array.ToList();
+            var result = list.Where(a => a % 2 != 0).OrderBy(a => a).ToList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                var nmbr = list[i];
+                if (nmbr % 2 == 0)
+                    result.Insert(i, nmbr);
+            }
+            return result.ToArray();
+        }
+
+        /*
+            Best solution:
+
+            Queue<int> odds = new Queue<int>(array.Where(e => e%2 == 1).OrderBy(e => e));
+            return array.Select(e => e%2 == 1 ? odds.Dequeue() : e).ToArray();
+         */
+    }
+}
---
+++ C-sharp/UnitTestProject/OddSortTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TheCode;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class OddSortTests
+    {
+        [Test]
+        public void OddSort_Basic1()
+        {
+            Assert.AreEqual(new int[] {1, 3, 2, 8, 5, 4}, OddSort.SortArray(new int[] {5, 3, 2, 8, 1, 4}));
+        }
+
+        [Test]
+        public void OddSort_Basic2()
+        {
+            Assert.AreEqual(new int[] { 1, 3, 5, 8, 0 }, OddSort.SortArray(new int[] { 5, 3, 1, 8, 0 }));
+        }
+
+        [Test]
+        public void OddSort_Empty()
+        {
+            Assert.AreEqual(new int[] { }, OddSort.SortArray(new int[] { }));
+        }
+
+        [Test]
+        public void OddSort_RecurringEvens_ShouldStay()
+        {
+            Assert.AreEqual(new int[] { 37, 84, 41, 49, 51, 57, 34, 57, 59, 6, 38, 78, 75, 77, 84, 18, 85, 87, 84, 99, 50 }, 
+                OddSort.SortArray(new int[] { 41, 84, 77, 49, 37, 57, 34, 57, 59, 6, 38, 78, 75, 51, 84, 18, 85, 87, 84, 99, 50 }));
+            
+        }
+    }
+}
